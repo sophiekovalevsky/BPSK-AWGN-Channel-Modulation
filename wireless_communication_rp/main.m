774cc340d77edd923d9165c7d12e07a10e9f6e63
Kiara Navarro <sophiekovalevsky@fedoraproject.org>
2014-08-18 01:48:43 -0500

    Create a new way to sample the signal using the corresponding Fs.

--- wireless_communication_rp/main.m
+++ wireless_communication_rp/main.m
@@ -4,21 +4,19 @@
 clear all;
 close all;
 
-% % Generate a binary sequence Pr(0) = 0.5 and Pr(1) = 0.5
-% binarySequence = rand(1,10)>0.5;
+% Generate a binary sequence Pr(0) = 0.5 and Pr(1) = 0.5
+binarySequence = rand(1,10e6)>0.5;
+binSeqLength = 10e6;
 
-% Load binary sequence from binary_sequence.mat
-sequenceStructure = load('binary_sequence');
-
-% Extract binary sequence fields
-binarySequence = cell2mat(extractfield(sequenceStructure, 'ip'));
-binSeqLength = length(binarySequence);
+% % Load binary sequence from binary_sequence.mat
+% sequenceStructure = load('binary_sequence');
+% 
+% % Extract binary sequence fields
+% binarySequence = cell2mat(extractfield(sequenceStructure, 'ip'));
+% binSeqLength = length(binarySequence);
 
 % Convert binary sequence to bipolar sequence
 bipolarSequence = 2*binarySequence-1;
-
-% Discrete time sequence bettwen 0 and 10*T with 1000 sampltes
-timeSequence = linspace(0, 5, 10000);
 
 % Bit duration in us
 bitDuration = 1; 
@@ -26,8 +24,11 @@ bitDuration = 1;
 % Sample frequency assigned by the assistant in MHz
 sampleFrequency = 5/bitDuration;
 
+% Duration of signal to generate in us
+T = bitDuration*binSeqLength;
+
 % Error bit used to normalized the amplitude of the signal
-erroBit = bitDuration/2;
+energyBit = bitDuration/2;
 
 % Values to BER in dB
 eBn0 = [0:12];
@@ -38,17 +39,25 @@ eBn0Max = max(eBn0);
 upsampleFactors = [1:4];
 upsFacLength = length(upsampleFactors);
 
-% Carrier frequency
+% Carrier frequency of 50 Hz
 carrierFrequency = 50;
 
 % Run for every unsampled factors
 for upsFacIndex = 1:upsFacLength
+
+	% Discrete time sequence
+	timeSequence = 1/(upsFacIndex*sampleFrequency):1/(upsFacIndex*sampleFrequency):T;
+	timSeqLength = length(timeSequence);
+
+	% Bipolar signal sampled
+	bipolarSampled = kron(bipolarSequence,ones(1,sampleFrequency));
+
 	% Upsample the unipolar sequence and get the vector length
-	upsampleSequence = upsample(bipolarSequence, upsampleFactors(upsFacIndex));
+	upsampleSequence = upsample(bipolarSampled, upsampleFactors(upsFacIndex));
 	upsSeqLength = length(upsampleSequence);
 
 	% Rectangular filter 
-	rectFilter = ones(1, upsampleFactors(upsFacIndex));
+	rectFilter = ones(1, upsampleFactors(upsFacIndex))
 
 	% Convolve upsample unipolar sequence with a rectangular filter
 	convSequence = 1/sqrt(upsampleFactors(upsFacIndex))*conv(upsampleSequence, rectFilter);
@@ -63,7 +72,7 @@ for upsFacIndex = 1:upsFacLength
 	moduSignal = seqFiltered;
 
 	% Signal modulated
-	%moduSignal = sqrt(2*errorBit/bitDuration)*cos(2*pi*carrierFrequency*t);
+	%moduSignal = sqrt(2*energyBit/bitDuration)*cos(2*pi*carrierFrequency*t);
 
 
 	% Run for every eBn0
@@ -75,7 +84,7 @@ for upsFacIndex = 1:upsFacLength
 		demSignalFilt = conv(transSignal,rectFilter); 
 
 		% I need to sample  the demodulated signal filtered 
-		demSignalSample = demSignalFilt(upsampleFactors(upsFacIndex):upsampleFactors(upsFacIndex):upsSeqLength); 
+		demSignalSample = demSignalFilt(upsampleFactors(upsFacIndex)*sampleFrequency:upsampleFactors(upsFacIndex)*sampleFrequency:upsSeqLength);
 
 		% Extract sequence using hard decision decoding with a threshold value of 0
 		binSeqDem = real(demSignalSample) > 0;
